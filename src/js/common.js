f26eed06b2deda10c142008dd1853f474c5f3aaf
designModiila <design@modiila.com>
2024-02-19 09:56:52 +0900

    폴더구성 기본작업

---
+++ src/js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#header').load('../../html/include/header.html')
+  $('#footer').load('../../html/include/footer.html')
+
+  $(".js-select2").select2({minimumResultsForSearch: -1});
+  
+  $("#file").on('change',function(){
+    var fileName = $("#file").val();
+    $(".upload-name").val(fileName);
+  });
+
+
+})
