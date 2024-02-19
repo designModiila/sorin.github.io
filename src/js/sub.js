168bad7b7455fc42f1ef064c22091395b6f96085
designModiila <design@modiila.com>
2024-02-19 18:11:15 +0900

    section02 수정

---
+++ src/js/sub.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+      slidesPerView: 1.7,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
