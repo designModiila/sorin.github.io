aa31604d40778b91eab4cf8e3eb73b64a52f00a0
designModiila <design@modiila.com>
2024-02-20 10:27:43 +0900

    슬라이드 수정

--- src/js/common.js
+++ src/js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('#header').load('../../html/include/header.html')
   $('#footer').load('../../html/include/footer.html')
 
-  $(".js-select2").select2({minimumResultsForSearch: -1});
+  // $(".js-select2").select2({minimumResultsForSearch: -1});
   
   $("#file").on('change',function(){
     var fileName = $("#file").val();
--- src/js/sub.js
+++ src/js/sub.js
@@ -2,7 +2,6 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
       type: "progressbar",
-      slidesPerView: 1.7,
     },
     navigation: {
       nextEl: ".swiper-button-next",
