1c5bc3abf5d75d17208eabaf3ebd3016a7aeb003
designModiila <design@modiila.com>
2024-02-20 17:25:50 +0900

    section0103 반응형

--- src/js/sub.js
+++ src/js/sub.js
@@ -1,4 +1,7 @@
 var swiper = new Swiper(".mySwiper", {
+   //loop: true,
+    slidesPerView: 1.2,
+  //   grabCursor: true,
     pagination: {
       el: ".swiper-pagination",
       type: "progressbar",
@@ -8,3 +11,31 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+//   const swiperTestmonials = new Swiper('.mySwiper', {
+//     loop: true,
+//     slidesPerView: 1.2,
+//     grabCursor: true,
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     breakpoints: {
+//         // when window width is >= 640px
+//         500: {
+//             slidesPerView: 1.4,
+//         },
+//         780: {
+//             slidesPerView: 1.8,
+//         },
+//         1300: {
+//             slidesPerView: 2.6,
+//         },
+//         1630: {
+//             slidesPerView: 3.2,
+//         }
+//     }
+
+// });
