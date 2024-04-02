7e7d766307e3cba3768208f4cfbd824922f514b1
JungHeekyung <kerryjhk@gmail.com>
2024-04-02 15:31:54 +0900

    sub 추가

--- resources/js/sub2.js
+++ resources/js/sub2.js
@@ -1,68 +1,62 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    // JavaScript를 이용하여 이미지 밝기를 분석하고 글자색을 조절
-    var mainTitleWrap = document.querySelector(".main-title-wrap");
-    var image = new Image();
-    image.src = getComputedStyle(mainTitleWrap).backgroundImage.replace(/url\(['"]?(.*?)['"]?\)/i, "$1");
+// document.addEventListener("DOMContentLoaded", function() {
+
+//     var mainTitleWrap = document.querySelector(".main-title-wrap");
+//     var image = new Image();
+//     image.src = getComputedStyle(mainTitleWrap).backgroundImage.replace(/url\(['"]?(.*?)['"]?\)/i, "$1");
   
-    image.onload = function() {
-      var canvas = document.createElement('canvas');
-      var ctx = canvas.getContext('2d');
-      ctx.drawImage(image, 0, 0);
-      var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-      var brightness = calculateBrightness(imageData); // 이미지 밝기 계산
-      var textColor = brightness < 10 ? "#fff" : "#000"; // 이미지 밝기에 따라 글자색 조절
+//     image.onload = function() {
+//       var canvas = document.createElement('canvas');
+//       var ctx = canvas.getContext('2d');
+//       ctx.drawImage(image, 0, 0);
+//       var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+//       var brightness = calculateBrightness(imageData); // 이미지 밝기 계산
+//       var textColor = brightness < 10 ? "#fff" : "#000"; // 이미지 밝기에 따라 글자색 조절
   
-      // 조절된 글자색을 적용
-      mainTitleWrap.style.color = textColor;
-    };
-  });
+//       mainTitleWrap.style.color = textColor;
+//     };
+//   });
   
-  function calculateBrightness(imageData) {
-    var sum = 0;
-    for (var i = 0; i < imageData.data.length; i += 4) {
-      sum += (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
-    }
-    return sum / (imageData.width * imageData.height);
-  }
+//   function calculateBrightness(imageData) {
+//     var sum = 0;
+//     for (var i = 0; i < imageData.data.length; i += 4) {
+//       sum += (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
+//     }
+//     return sum / (imageData.width * imageData.height);
+//   }
 
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    var mainTitleWrap = document.querySelector(".main-title-wrap");
+// document.addEventListener("DOMContentLoaded", function() {
+//     var mainTitleWrap = document.querySelector(".main-title-wrap");
   
-    gsap.to(".main-visual", {
-      duration: 2,
-      y: -485,
-      borderRadius: 0,
-      margin: 0,
+//     gsap.to(".main-visual", {
+//       duration: 2,
+//       y: -485,
+//       borderRadius: 0,
+//       margin: 0,
   
-      scrollTrigger: {
-        trigger: ".section01",
-        start: "top top",
-        end: "bottom top",
-        pin:true,
-        scrub: 1,
-      
-      }
-    });
-  });
-
-
-//   gsap.to(".main-visual", {
-//     duration: 2,
-//     y: -485,
-//     borderRadius: 0,
-//     margin: 0,
-
-//     scrollTrigger: {
+//       scrollTrigger: {
 //         trigger: ".section01",
 //         start: "top top",
 //         end: "bottom top",
 //         pin:true,
-//         scrub: 1,    
-//         markers: true,
-//         pinSpacing: 10,
-//     }
-// });
+//         scrub: 1,
+      
+//       }
+//     });
+//   });
+
+// let tl = gsap.timeline({delay: 0.7});
+// tl.to('.text-ani', {duration:0.5, y:0, stagger: 0.3});
+
+
+ScrollTrigger.create ({
+  trigger:".mission-wrap",
+  start:"top top",
+  end:"bottom bottom",
+  pin:".mission-right",
+  scrub:2,
+  // markers:true,
+})
