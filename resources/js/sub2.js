5c02d01ae479d45b4920cf8cdd79c7bf1e06930c
JungHeekyung <kerryjhk@gmail.com>
2024-04-12 14:09:54 +0900

    sub01 모션추가

--- resources/js/sub2.js
+++ resources/js/sub2.js
@@ -1,66 +1,4 @@
 
-// document.addEventListener("DOMContentLoaded", function() {
-
-//     var mainTitleWrap = document.querySelector(".main-title-wrap");
-//     var image = new Image();
-//     image.src = getComputedStyle(mainTitleWrap).backgroundImage.replace(/url\(['"]?(.*?)['"]?\)/i, "$1");
-  
-//     image.onload = function() {
-//       var canvas = document.createElement('canvas');
-//       var ctx = canvas.getContext('2d');
-//       ctx.drawImage(image, 0, 0);
-//       var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-//       var brightness = calculateBrightness(imageData); // 이미지 밝기 계산
-//       var textColor = brightness < 10 ? "#fff" : "#000"; // 이미지 밝기에 따라 글자색 조절
-  
-//       mainTitleWrap.style.color = textColor;
-//     };
-//   });
-  
-//   function calculateBrightness(imageData) {
-//     var sum = 0;
-//     for (var i = 0; i < imageData.data.length; i += 4) {
-//       sum += (imageData.data[i] + imageData.data[i + 1] + imageData.data[i + 2]) / 3;
-//     }
-//     return sum / (imageData.width * imageData.height);
-//   }
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     var mainTitleWrap = document.querySelector(".main-title-wrap");
-  
-//     gsap.to(".main-visual", {
-//       duration: 2,
-//       y: -485,
-//       borderRadius: 0,
-//       margin: 0,
-  
-//       scrollTrigger: {
-//         trigger: ".section01",
-//         start: "top top",
-//         end: "bottom top",
-//         pin:true,
-//         scrub: 1,
-      
-//       }
-//     });
-//   });
-
-// let tl = gsap.timeline({delay: 0.7});
-// tl.to('.text-ani', {duration:0.5, y:0, stagger: 0.3});
-
-
-// ScrollTrigger.create ({
-//   trigger:".mission-wrap",
-//   start:"top top",
-//   end:"bottom bottom",
-//   pin:".mission-right",
-//   scrub:2,
-//   // markers:true,
-// })
-
 
 //메인 서브 비주얼
 
@@ -73,13 +11,12 @@ mainVisual.from(".main-visual", {width: "40vw", x: "50vw"})
           .from(".main-visual", {y: visualValue})
           .from(".main-title-wrap", {y: -visualValue},"<")
           .to(".main-title-wrap", {color: "#fff"},"<");
-          // .to(".main-title-wrap", {y: visualValue, color: "#fff"});
 
 ScrollTrigger.create({
     animation: mainVisual,
     trigger: ".section01",
     start: "top top",
-    stagger: 10,
+    stagger: 0.3,
     ease: "power3.in",
     scrub: 1,
     pin: true,
@@ -166,18 +103,19 @@ document.getElementById("defaultOpen").click();
 
 
 
-const cont = gsap.utils.toArray('.cont')
-gsap.set(cont,{y: '25%', opacity: 0})
-cont.forEach(cont => {
-  gsap.to(cont,{
-    y: 0,
-    autoAlpha: 1,
-    duration: 0.5,
-    stagger: 0.3,
-    scrollTrigger: {
-      trigger: cont,
-      start: 'top 70%',
-      toggleActions:"restart none none reverse"
-    }
-  })
-})
+// const cont = gsap.utils.toArray('.cont');
+// gsap.set(cont,{y: '25%', opacity: 0})
+// cont.forEach(cont => {
+//   gsap.to(cont,{
+//     y: 0,
+//     autoAlpha: 1,
+//     duration: 0.5,
+//     stagger: 0.3,
+//     scrollTrigger: {
+//       trigger: cont,
+//       start: 'top 70%',
+//       toggleActions:"restart none none reverse",
+//     }
+//   })
+// });
+
