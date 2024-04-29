c6d676b4380d7fac7ce5a6dab2345108a36919de
JungHeekyung <kerryjhk@gmail.com>
2024-04-29 15:31:57 +0900

    인프라 슬라이드 수정

--- resources/js/sub.js
+++ resources/js/sub.js
@@ -115,19 +115,37 @@ new Swiper("#js-swiper-infra", {
           }
         }, this);
       }
-    },
-    // breakpoints: {
-    //             300: {
-    //                 slidesPerView: 1,
-    //             },
-    //             768: {
-    //                 slidesPerView: 2,
-    //             },
-    //         }
+    }
   });
 
 
-  
+  new Swiper('#js-swiper-hardware', {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 230,
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        // disableOnInteraction: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    observer: true,
+    observeParents: true,
+    breakpoints: {
+        300: {
+            slidesPerView: 1.5,
+            spaceBetween: 80,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 230,
+        },
+    },
+  });
+
 
   new Swiper(".backup-slide", {
     slidesPerView: 1,
